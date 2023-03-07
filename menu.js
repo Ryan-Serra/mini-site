d0a2992da05cb958619668172b726d9f1c4bf411
Ryan-Serra <120741341+Ryan-Serra@users.noreply.github.com>
2023-03-07 14:38:50 -0300

    Add files via upload
    Aqui está um dos meus primeiros projetos  responsivos

---
+++ menu.js
@@ -0,0 +1,10 @@
+var a = document.querySelector('.menu')
+function ClickMenu(){
+     if (a.style.display=='block'){
+        a.style.display='none'
+     }
+     else{
+        a.style.display='block'
+     }
+    }
+    
